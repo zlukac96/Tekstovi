f74c967d2b17137f29a7e18f26dc3326b90d107e
zlukac96 <97309080+zlukac96@users.noreply.github.com>
2024-12-20 22:23:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -236,4 +236,11 @@
 
 	inputTextSearch.addEventListener("input", songSearch);
 
+document.addEventListener('touchmove', function(event) {
+    if (event.target.closest('.scrollable')) {
+        event.stopPropagation(); // Onemogućava skrolovanje na celokupnoj stranici
+    }
+});
+
+
 //});
